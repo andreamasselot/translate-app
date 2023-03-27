b3613ac04d2629ccab8d882c1d6dfafae0eb0288
Andréa <andrea@MacBook-Air-de-Andrea.local>
2023-03-27 14:03:46 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [languages, setLanguages] = useState("")
+  const [translated, setTranslated] = useState("")
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="title">
+          <h1>Translate</h1>
+        </div>
+        <section className="translator-container">
+          <div>
+            <input type="text" name="original-text" id="original-text" placeholder="Enter your text ..."/>
+            <textarea name="translated-text" id="translated-text" cols="30" rows="10"></textarea>
+            <div className="languages-buttons-container">
+              <button>French</button>
+              <button>English</button>
+              <button>Spanish</button>
+              <button>German</button>
+              <button>Portuguese</button>
+              <button>Italian</button>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
